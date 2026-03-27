892b46ee59e0fb36a07e0d0be5649e4b740bee29
Samarth Vohra <samarthvohra@Samarths-MacBook-Air.local>
2026-03-27 20:13:27 +0530

    node lecture 5 code

---
+++ node-5/swiggy_backend/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+// local DB
+// mongoose.connect('mongodb://127.0.0.1:27017/samosa') //promise
+// cloud DB
+mongoose.connect('mongodb+srv://samarthvohraindia_db_user:5kx1J5WLVYtB0CCx@cluster0.j7nop0u.mongodb.net/') 
+.then(()=>{
+    console.log('DB CONNECTED cloud')
+})
+.catch((ERR)=>{
+    console.log('DB NOT CONNECTED' , ERR) 
+})
+
+const PORT = 8080;
+app.listen(PORT , ()=>{
+    console.log(`Server connected at port: ${PORT}`);
+})
